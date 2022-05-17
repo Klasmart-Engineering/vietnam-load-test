cc29cc0b385c3823119822b906aa2a22f30749e5
Hai Nguyen <hai.nguyen@kidsloop.live>
2022-05-17 17:37:54 +0700

    FEAT: add script to copy screenshots from pod to local

---
+++ k8s/copy-screenshots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -aeuo pipefail
+
+# create the dotenv file if it doesn't exist
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+source .env
+
+#kubectl delete job -n ${K8S_NAMESPACE} $(kubectl get jobs -n ${K8S_NAMESPACE} | grep ${JOB_NAME} | awk '{print $1}')
+
+PODS_LIST=($(kubectl get pods -n ${K8S_NAMESPACE} | grep ${JOB_NAME} | awk '{print $1}'))
+
+for pod in "${PODS_LIST[@]}"
+do
+	kubectl cp -n ${K8S_NAMESPACE} $pod:/mnt/nightwatchjs/screens screens
+done
