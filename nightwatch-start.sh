eba94884ad5ca2cca80da77165842af2dfdcdce4
Hai Nguyen <hai.nguyen@kidsloop.live>
2022-05-17 15:32:53 +0700

    FEAT: update container start script, add some variables

--- nightwatch-start.sh
+++ nightwatch-start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -aeuo pipefail
 
-rm -f ./screens/* & \
-/usr/bin/Xvfb -ac :99 -screen 0 1920x1080x32 > /dev/null 2>&1 & \
-echo ${DISPLAY} & \
-npm run testInHouseStay30MinutesInLive
+[ -z $DISPLAY ] && DISPLAY=":99.0"
+[ -z $RESOLUTION ] && RESOLUTION="1920x1080x32"
+[ -z $TEST_NAME ] && TEST_NAME="testInHouseStay30MinutesInLive"
+
+rm -f ./screens/*
+echo "DISPLAY: ${DISPLAY}"
+echo "RESOLUTION: ${RESOLUTION}"
+echo "TEST_NAME: ${TEST_NAME}"
+/usr/bin/Xvfb -ac ${DISPLAY} -screen 0 ${RESOLUTION} > /dev/null 2>&1 & \
+npm run ${TEST_NAME}
