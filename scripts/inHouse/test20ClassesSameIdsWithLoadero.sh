a4f0310e7bffd73be831eec703009530785fa3b7
Hung Huynh <hung.huynh@kidsloop.live>
2022-05-19 16:55:27 +0700

    revert: don't need to script test20ClassesSameIdsWithLoadero

--- scripts/inHouse/test20ClassesSameIdsWithLoadero.sh
+++
@@ -1,9 +0,0 @@
-#!/bash/bash
-
-ROOM_ID_LIST="$(npm run getLoaderoRoomIds --silent)";
-
-for i in $ROOM_ID_LIST; do
-    echo $i;
-done
-
-# curl -X POST  https://api.loadero.com/v2/projects/10560/tests/14341/runs/ -H "Authorization: Bearer YJE0NGFJYMETM2MWNY0ZMWY4LTGXYTCTMWE5OGQ0OWM5YZUX"
