2a04a91bf93491adae302f7de14c514f5a9c1132
Hai Nguyen <hai.nguyen@kidsloop.live>
2022-05-20 08:24:45 +0700

    FEAT: run 1 assistant teacher on loadero for each classroom

--- k8s/runK8SJobs.sh
+++ k8s/runK8SJobs.sh
@@ -3,26 +3,38 @@ set -aeuo pipefail
 
 # create the dotenv file if it doesn't exist
 if [ ! -f .env ]; then
-  cp .env.example .env
+	cp .env.example .env
 fi
 
 source .env
 
-for class in $(seq 1 $CONCURRENT_CLASSES);
+## Run nightwatchJS jobs
+for class in $(seq 0 $((CONCURRENT_CLASSES-1)));
 do	
 	room_id="class${class}"
 	teacher_id="teacher${room_id}"
 	echo "=========="
-	echo "Participant: ${teacher_id}, class: ${room_id}"
-	# run teacher job here
+	echo "Participant: ${teacher_id}, class: ${room_id}"	
+	### Run teacher job
 	./generateK8SJob.sh teacher $room_id $teacher_id
 
 	for student in $(seq 1 $STUDENTS);
 	do
 		student_id="${room_id}student${student}"
 		echo "=========="
-    		echo "Participant: ${student_id}, class: ${room_id}, teacher: ${teacher_id}"
-			# run student job here
-			./generateK8SJob.sh student $room_id $student_id
+    	echo "Participant: ${student_id}, class: ${room_id}, teacher: ${teacher_id}"
+		### Run student job
+		./generateK8SJob.sh student $room_id $student_id
 	done
 done
+
+## Run assistant teacher on Loadero
+### change the number of concurrent classes on loadero
+curl -X PUT "https://api.loadero.com/v2/projects/${LOADERO_PROJECT_ID}/tests/${LOADERO_TEST_ID}/groups/${LOADERO_GROUP_ID}/" \
+-d "{\"name\": \"Class\", \"count\": ${CONCURRENT_CLASSES}}" \
+-H "Content-Type: application/json" \
+-H "Authorization: LoaderoAuth ${LOADERO_ACCESS_TOKEN}"
+
+### Run loadero
+curl -X POST  "https://api.loadero.com/v2/projects/${LOADERO_PROJECT_ID}/tests/${LOADERO_TEST_ID}/runs/" \
+-H "Authorization: LoaderoAuth ${LOADERO_ACCESS_TOKEN}"
