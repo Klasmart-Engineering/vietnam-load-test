28a194c3596a728f9a23726589e16faaed486e36
Hung Huynh <hung.huynh@kidsloop.live>
2022-05-26 14:50:11 +0700

    refactor: report directory separate by participant

--- html-reporter.js
+++ html-reporter.js
@@ -2,8 +2,8 @@ var HtmlReporter = require('nightwatch-html-reporter');
 /* Same options as when using the built in nightwatch reporter option */
 var reporter = new HtmlReporter({
   openBrowser: false,
-  reportsDirectory: `${__dirname}/reports/${process.env.RUN_TIME}-${process.env.TEST_NAME}` ,
-  reportFilename: process.env.PARTICIPANTID + '_selenium_report.html',
+  reportsDirectory: `${__dirname}/reports/${process.env.RUN_TIME}-${process.env.TEST_NAME}/${process.env.PARTICIPANTID}` ,
+  reportFilename: 'selenium_log.html',
   uniqueFilename: false,
 });
  
